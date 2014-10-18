13dd685fcca2f2a27942203d669712eb144be698
Tihomir Trifonov <ttrifonov@cloudrunner.io>
2014-10-18 17:03:33 +0300

    improved release labeling
    Change-Id: I3c6e77272266068cd0066aa1a70914525357bf0d

--- scripts/rpm/getrev.sh
+++ scripts/rpm/getrev.sh
@@ -2,7 +2,7 @@
 
 MAJOR=$(python -c "from cloudrunner_server.version import VERSION; print VERSION");
 if [ "$(git branch |grep "\*" |grep master )" ] ;then
-    MINOR=$(git log --pretty=format:'%ad %h %d' --abbrev-commit --date=iso -1|awk {'print $1"_"$2"."$4'}|sed -e 's/-/_/g' | sed -e 's/\:/./g');
+    MINOR=$(git log --pretty=format:'%ad %h %d' --abbrev-commit --date=iso -1|awk {'print $1"-"$2"_"$4'}|sed -e 's/[-:]//g');
 else
     MINOR=$(git branch -r | grep stable/ | sort | tail -n1 | cut -d "/" -f3);
 fi
