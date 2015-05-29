15ea14b08402c08bdfd129f08bea114aefecc32a
Tihomir Trifonov <ttrifonov@cloudrunner.io>
2015-05-29 17:43:35 +0300

    Updated args for amazon provider
    Change-Id: I44f49c2521062426fd4ad3cc63948e32f43fb8bd

--- cloudrunner_server/plugins/clouds/amazon.py
+++ cloudrunner_server/plugins/clouds/amazon.py
@@ -23,7 +23,7 @@ LOG = logging.getLogger()
 class AWS(BaseCloudProvider):
 
     def create_machine(self, name, region='us-west-2',
-                       image=None, inst_type=None,
+                       image=None, size=None,
                        min_count=1, max_count=1,
                        server=CR_SERVER,
                        security_groups=None,
@@ -31,6 +31,8 @@ class AWS(BaseCloudProvider):
         LOG.info("Registering AWS machine [%s::%s] for [%s]" %
                  (name, image, CR_SERVER))
         try:
+            if image == 'custom':
+                image = kwargs['custom-image']
             self.conn = ec2.connect_to_region(
                 region,
                 aws_access_key_id=self.profile.username,
@@ -42,7 +44,7 @@ class AWS(BaseCloudProvider):
                 image,
                 min_count=min_count, max_count=max_count,
                 user_data="#!/bin/bash\n\n%s" % boot_cmd,
-                instance_type=inst_type,
+                instance_type=size,
                 key_name=key_name,
                 security_groups=security_groups)
 
@@ -52,6 +54,8 @@ class AWS(BaseCloudProvider):
             return self.OK, instance_ids, meta
         except Exception, ex:
             LOG.exception(ex)
+            LOG.error("DATA: %s :: %s :: %s :: %s" % (
+                region, image, size, kwargs))
             return self.FAIL, [], {}
 
     def delete_machine(self, instance_ids, region='us-west-2', **kwargs):
